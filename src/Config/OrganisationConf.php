199a1c685e9c9333c6c02a6dbe61d3f87bb3f8a2
Pritam Bag <pritam02b@gmail.com>
2025-12-29 11:22:14 +0530

    enhancement: added api configuration

--- src/Config/OrganisationConf.php
+++ src/Config/OrganisationConf.php
@@ -3,6 +3,7 @@
 namespace Iquesters\Organisation\Config;
 
 use Iquesters\Foundation\Support\BaseConf;
+use Iquesters\Foundation\Support\ApiConf;
 use Iquesters\Foundation\Enums\Module;
 
 class OrganisationConf extends BaseConf
@@ -10,11 +11,18 @@ class OrganisationConf extends BaseConf
     // Inherited property of BaseConf, must initialize
     protected ?string $identifier = Module::ORGANISATION;
     
+    
     // properties of this class
     protected string $organisation_layout;
+    
+    protected ApiConf $api_conf;
     
     protected function prepareDefault(BaseConf $default_values)
     {
         $default_values->organisation_layout = 'organisation::layouts.package';
+
+        $default_values->api_conf = new ApiConf();
+        $default_values->api_conf->prefix = 'organisation'; // Must be auto generated from module enum - the vendor name  
+        $default_values->api_conf->prepareDefault($default_values->api_conf);
     }
 }
