f3b9627b52abfca6af5b66de82c2a4ad3fcd177e
Sambuddha Chatterjee <samhere17@gmail.com>
2025-12-30 18:19:01 +0530

    enhancement: restructuring the PackageInfo, ServiceProvider

--- src/OrganisationServiceProvider.php
+++ src/OrganisationServiceProvider.php
@@ -1,25 +1,10 @@
 <?php
 
-namespace Iquesters\Organisation;
+namespace Iquesters\Organisation\Providers;
 
 use Iquesters\Foundation\System\Providers\BaseServiceProvider;
-use Iquesters\Foundation\System\Package\PackageInfo;
-use Iquesters\Organisation\Package\OrganisationPackageInfo;
 
 class OrganisationServiceProvider extends BaseServiceProvider
 {
-    protected function packageInfo(): PackageInfo
-    {
-        return new OrganisationPackageInfo();
-    }
-
-    // protected function seedCommandName(): string
-    // {
-    //     return 'organisation';
-    // }
-
-    // protected function seederClass(): string
-    // {
-    //     return OrganisationSeeder::class;
-    // }
+    // Everything auto-detected and handled by BaseServiceProvider!
 }
--- src/Package/OrganisationPackageInfo.php
+++ src/Package/OrganisationPackageInfo.php
@@ -6,7 +6,23 @@ use Iquesters\Foundation\System\Package\PackageInfo;
 
 class OrganisationPackageInfo extends PackageInfo
 {
-    protected string $module_name = 'organisation';
+    protected function definePackageInfo(): void
+    {
+        $this->laravel_config_name = 'organisation';
 
-    protected string $seeder_class = 'OrganisationSeeder';
+        $this->specific_providers = [
+            OrganisationServiceProvider::class,
+        ];
+
+        $this->specific_commands = [
+            OrganisationSeederCommand::class,
+        ];
+
+        $this->specific_models = [
+            Organisation::class,
+            Department::class,
+        ];
+
+        $this->custom_views_path = $this->getPackagePath() . '/custom/views';
+    }
 }
